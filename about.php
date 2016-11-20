0ce70db99b0d7e5da86b194dc35212bcf7df05db
Ahmad Mushtaq <myself@ahmadmushtaq.com>
2016-11-20 11:22:05 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome to Student Information Center:: Institute of Computing, BZU</title>
+<style type="text/css">
+<!--
+.heading {
+	color: #F90;
+	font-family: "Comic Sans MS", cursive;
+}
+.options {
+	font-family: "Comic Sans MS", cursive;
+	font-size: 16px;
+	font-style: oblique;
+	color: #F93;
+}
+-->
+</style>
+</head>
+
+<body background="admin/images/1019286_abstract_orange_tiles_background_.jpg">
+
+<br />
+<br />
+<br />
+<table align="center" cellpadding="0" bgcolor="#FFFFFF" width="800" border="0">
+  <tr>
+    <td><p align="center"><img src="admin/images/cooltext458256871.png" width="170" height="83" alt="About" /></p>
+      <table align="center" width="759" border="0">
+        <tr>
+          <td width="160" height="287"><img src="admin/images/ahmadpic.jpg" width="160" height="200" /></td>
+          <td valign="top" width="589"><ul>
+            <li><strong>Project Information</strong>:</li>
+            <ul>
+              <li>This is a simple <strong>Student Information System</strong> where the user can view, store, modify and delete a student's information. It has been developed by <strong>Ahmad Mushtaq</strong> as a class assignment using <strong>PHP </strong>&amp; <strong>MySQL.</strong> It was assigned by our respectable teacher <strong>Sir Ahsan Raza.</strong></li>
+            </ul><br />
+            <li><strong>Student Information:</strong></li>
+            <ul>
+              <li><strong>Name: </strong><a target="_blank" href="http://www.ahmadmushtaq.com">Ahmad Mushtaq</a></li>
+              <li><strong>Homepage : </strong><a href="http://www.ahmadmushtaq.com" target="_blank" >www.AhmadMushtaq.com</a></li>
+              <li><strong>Roll No : </strong>07-45</li>
+              <li><strong>Program: </strong>BS-IT</li>
+              <li><strong>Session: </strong>2007-11</li>
+              <li><strong>Department: </strong>Information Technology</li>
+              <li><strong>Institute: </strong><a target="_blank" href="http://www.bzu.edu.pk/computer/">Institute of Computing</a>, <a href="http://www.bzu.edu.pk" target="_blank" >Bahauddin Zakariya University </a>, <a href="http://hamaramultan.com/" target="_blank">Multan</a></li>
+              <li><strong>Course: </strong>Web Engineering</li>
+              <li><strong>Course Instructor: </strong><a href="http://bzu.edu.pk/php/departments/staff.php?departmentcode=4" target="_blank">Sir Ahsan Raza</a></li>
+            </ul>
+          </ul></td>
+        </tr>
+      </table>
+<table width="70%" align="center">
+<tr><td>
+<h2>LICENCE/WARRANTY</h2>
+<p>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:</p>
+</br></br><p>
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+</p></br></br><p>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL <b><font color="red">AHMAD MUSHTAQ</font></b> BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+</p>
+</td></tr></table>
+      <p align="center"><a href="./"><img border="0" src="admin/images/cooltext457951462.png" alt="Go Back" onmouseover="this.src='admin/images/cooltext457951462MouseOver.png';" onmouseout="this.src='admin/images/cooltext457951462.png';" /></a><a href="admin/index.php"><img border="0" src="admin/images/cooltext457954941.png" onmouseover="this.src='admin/images/cooltext457954941MouseOver.png';" onmouseout="this.src='admin/images/cooltext457954941.png';" /></a><a href="http://www.ahmadmushtaq.com"><img border="0" src="admin/images/cooltext457955210.png" onmouseover="this.src='admin/images/cooltext457955210MouseOver.png';" onmouseout="this.src='admin/images/cooltext457955210.png';" /></a></p>
+      <center>
+        <a href="about.php"><img src="admin/images/ahmad.jpg" alt="Developed by Ahmad Mushtaq" width="395" height="32" border="0" /></a>
+      </center>
+    <p align="left">&nbsp;</p></td>
+  </tr>
+</table>
+<h1 align="center" class="heading">&nbsp;</h1>
+</body>
+</html>
